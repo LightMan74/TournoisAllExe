cb4d17a13f109212e0e14bd2dc6af224c4a00d1a
Lansard <williamlansard@live.fr>
2023-06-09 01:58:17 +0200

    3.08

--- TournoisAllExe/Form1.Designer.vb
+++ TournoisAllExe/Form1.Designer.vb
@@ -142,7 +142,7 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximumSize = New System.Drawing.Size(481, 404)
         Me.Name = "Form1"
-        Me.Text = "TournoiAllExe 3.07"
+        Me.Text = "TournoiAllExe 3.08"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
